ca6cb1919efab4957e706684567352b482dfacf7
15575132136 <1405111813@qq.com>
2023-06-01 21:52:42 +0800

    添加mybatis

--- login/src/main/java/com/hmx/jxc/controller/UserController.java
+++ login/src/main/java/com/hmx/jxc/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2023/5/27
  **/
 @RestController
-public class TestController {
+public class UserController {
     /**
      * @Autowired 功能： 赋值
      */
---
+++ product/src/main/java/com/hmx/jxc/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.hmx.jxc.controller;
+
+/**
+ * @author 14051
+ * @date 2023/6/1
+ **/
+public class ProductController {
+
+
+
+
+
+}
